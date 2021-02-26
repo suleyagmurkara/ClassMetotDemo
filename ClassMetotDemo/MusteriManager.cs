3e5b0907e39f4ea0ae56a9db93fd1ba99247e1ab
ŞULE YAĞMUR KARA <2015010205007@ogrenci.karabuk.edu.tr>
2021-02-27 00:26:59 +0300

    26.02.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi!");
+            Console.WriteLine("Adı: " + musteri.Ad);
+            Console.WriteLine("Soyadı: " + musteri.Soyad);
+            Console.WriteLine("Telefon: " + musteri.Telefon);
+            Console.WriteLine("------------------------------");
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,27 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Şule";
+            musteri1.Soyad = "KARA";
+            musteri1.Telefon = 5405555555;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Yağmur";
+            musteri2.Soyad = "KARA";
+            musteri2.Telefon = 5322222222;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Betül";
+            musteri3.Soyad = "KARA";
+            musteri3.Telefon = 5687471489;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
         }
     }
 }
